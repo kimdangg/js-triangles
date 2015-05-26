fa33c5c41bbbcb935327a495a5b2c2b0fb494252
Guest User <student@epicodus.com>
2015-05-26 10:31:38 -0700

    Write function to check if a triangle is either an equilateral, isoceles, scalene or not a triangle at all

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var triangle = function(side1, side2, side3) {
+  if ((side1 + side2 < side3) || (side1 + side3 < side2) || (side2 + side3 < side1)) {
+    return "not a triangle";
+  } else if ((side1 === side2) && (side2 === side3)) {
+    return "equilateral";
+  } else if ((side1 === side2) || (side2 === side3) || (side1 === side3)) {
+    return "isoceles";
+  } else {
+    return "scalene";
+  }
+};
---
+++ spec/specs.js
@@ -0,0 +1,17 @@
+describe('triangle', function() {
+  it('is equilateral if it is an equilateral triangle', function() {
+    expect(triangle(3,3,3)).to.eq("equilateral");
+  });
+
+  it('is isoceles if it is an isoceles triangle', function() {
+    expect(triangle(2, 2, 3)).to.eq("isoceles");
+  });
+
+  it('is scalene if it is a scalene triangle', function() {
+    expect(triangle(2, 4, 3)).to.eq("scalene");
+  });
+
+  it('is not a triangle if it is not a valid triangle', function() {
+    expect(triangle(2, 7, 3)).to.eq("not a triangle");
+  });
+});
