81b3bc03f99f2de064753f667a26ac080715407d
Guest User <student@epicodus.com>
2015-05-26 10:50:57 -0700

    Add functional website pages

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,24 @@ var triangle = function(side1, side2, side3) {
   if ((side1 + side2 < side3) || (side1 + side3 < side2) || (side2 + side3 < side1)) {
     return "not a triangle";
   } else if ((side1 === side2) && (side2 === side3)) {
-    return "equilateral";
+    return "an equilateral";
   } else if ((side1 === side2) || (side2 === side3) || (side1 === side3)) {
-    return "isoceles";
+    return "an isoceles";
   } else {
-    return "scalene";
+    return "a scalene";
   }
 };
+
+$(document).ready(function() {
+  $("form#triangle_sides").submit(function(event) {
+    var side1 = parseInt($("input#side1").val());
+    var side2 = parseInt($("input#side2").val());
+    var side3 = parseInt($("input#side3").val());
+    var result = triangle(side1, side2, side3);
+    $(".triangle").text(result);
+    $("#result").show();
+
+    event.preventDefault();
+
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,14 +1,14 @@
 describe('triangle', function() {
   it('is equilateral if it is an equilateral triangle', function() {
-    expect(triangle(3,3,3)).to.eq("equilateral");
+    expect(triangle(3,3,3)).to.eq("an equilateral");
   });
 
   it('is isoceles if it is an isoceles triangle', function() {
-    expect(triangle(2, 2, 3)).to.eq("isoceles");
+    expect(triangle(2, 2, 3)).to.eq("an isoceles");
   });
 
   it('is scalene if it is a scalene triangle', function() {
-    expect(triangle(2, 4, 3)).to.eq("scalene");
+    expect(triangle(2, 4, 3)).to.eq("a scalene");
   });
 
   it('is not a triangle if it is not a valid triangle', function() {
